cebc23b3201357a3a2d9abcd5e75c6076d12bacd
vinay961 <vinay100rai@gmail.com>
2024-01-11 14:16:56 +0530

    Basic Python

---
+++ 1_python.py
@@ -0,0 +1,64 @@
+# x = str(3)
+# y = int(3)
+# z = float(3)
+
+# print(type(x),y,z)
+
+# Note -> String can be declared with single or double quotes.
+
+# x,y,z = "abc","bcd","cde"
+# print(x)
+# print(y)
+# print(z)
+# Each variable get the respective values that we give earlier.
+
+# x = y = z = "abc"
+# print(x)
+# print(y)
+# print(z)
+# Each variable get the same value that we declare above.
+
+
+# List items given to variables
+# furits = ["Apple","Banana","Orange","Pineapple","Pea"]
+# a,b,c = furits   # we get error that too many values to unpack.
+# a,b,c,d,e = furits # Now its run correctly.
+# print(a)
+# print(b)
+# print(c)
+
+
+# Global Variable
+# x="Python"
+# def myFun() :
+#     x = "python"
+#     print("I Love " + x)
+    
+# def myFun1() :
+#     print("I want to learn " + x) # here we get x is not defined.
+    
+# myFun()
+# myFun1() 
+
+# def myFun() :
+#     # Making x a global varible
+#     global x
+#     x = "python"
+#     print("I Love " + x)
+    
+# def myFun1() :
+#     print("I want to learn " + x)
+    
+# myFun()
+# myFun1()
+
+
+#  Conditional Statements
+# a = 1
+# b = 1
+# if a==b:
+#     print("Both are equal")
+# else: 
+#     print("Both are unequal")    
+
+   
--- sample.py
+++
@@ -1,5 +0,0 @@
-x = str(3)
-y = int(3)
-z = float(3)
-
-print(type(x),y,z)
