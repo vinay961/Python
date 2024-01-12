b0a1ae28e9ffd96a57bba5f5590dc649224ab286
vinay961 <vinay100rai@gmail.com>
2024-01-12 10:01:42 +0530

    Loop and Conditional Statement

---
+++ 2_python.py
@@ -0,0 +1,39 @@
+# how to use else if in python
+
+# a = 10
+# b = 20
+# if a==b :
+#     print("Both numbers are equal.")
+    
+# elif a>b:
+#     print("First number is greater then Second one.")
+    
+# else:
+#     print("Second number is greater then first.")
+
+
+# Loop in python
+
+# i = 1
+# while i <= 5 :
+#     print( i+1 )
+#     if i == 3:
+#         break
+#     i+=1
+
+# fruits = ["apple","Mango","Orange","pineApple",]
+# for x in fruits:
+#     print(x)
+
+# for x in "Vinay":
+#     print(x)
+
+# for x in range(5):  # start from zero and continue till n-1
+#     print(x)  
+# else:
+#     print("Loop completed")
+
+# Past Statement in python -> if we don't want to write anything in for loop then we can use pass
+
+# for x in "Vinay":
+#     pass           when we use pass statement it doesn't return any error.
