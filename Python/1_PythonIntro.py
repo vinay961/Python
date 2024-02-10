5f8a90f8e7c82d4e004773daa77c4ab1db1cf222
vinay961 <vinay100rai@gmail.com>
2024-02-10 20:34:09 +0530

    Intro

---
+++ Python/1_PythonIntro.py
@@ -0,0 +1,38 @@
+# Here we have to study about basic python and how it work
+
+# Python is dynamically typed language, we don't need to declare variable type it get automatically assign inside memory. a=10  
+# a = 10
+# print(a)
+# a="Vinay"
+# print(a) 
+# In this example we can see that a work as number datatype for some instance and for other instance it act as string type, that why we say that datatype is not assigned to a but it assigned to 10 and that word inside memory.
+
+# if you want to specify the data type of variable then we can use casting.
+# x = str(3)
+# y = int(3)
+# z = float(3)
+# print(type(x))
+
+# Variables are case-sensitive
+# a = 10
+# A = "Vinay"
+# print(A)
+# print(a)
+
+# Some important type of question 
+# print(5+3)
+# print('ram'+'shyam')
+# print(2,3)
+# print(2+"Vinay") It gives us error bcz we cann't concatenate the diffirent data types with each other.
+
+# Note--> That we can use the global keyword to declare any variable global.
+
+# Datatypes --> Mutable == list,dictionary,set,bytesArray && Immutable == Integer,float,complex,string,tupes,frozen set,bytes
+# Now to understand the concept of immutable and mutable we have to understand the inner working python
+# a = 10
+# b = a
+# print(a,b) # output-> 10 10
+# b = 20
+# print(a,b) # output-> 10 20
+
+             
