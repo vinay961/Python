6aa3a966d840e70424338bf358a3f8590bfa71ce
vinay961 <vinay100rai@gmail.com>
2024-04-30 11:42:30 +0530

    some changes

--- project/index.py
+++ project/index.py
@@ -1,7 +1,7 @@
 from reportlab.platypus import SimpleDocTemplate, Table, Paragraph, TableStyle 
 from reportlab.lib import colors 
 from reportlab.lib.pagesizes import A4 
-from reportlab.lib.styles import getSampleStyleSheet 
+from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
 
 # Function to get item details from user
 def get_item_details():
@@ -42,7 +42,6 @@ def generate_receipt():
 
     # Fetching the style of Top level heading (Heading1) 
     title_style = styles["Heading1"] 
-
     title_style.alignment = 1
 
     # Creating the paragraph with the heading text and passing the styles of it 
@@ -50,19 +49,17 @@ def generate_receipt():
 
     # Creates a Table Style object and defines the styles row-wise 
     style = TableStyle([ 
-        ("BOX", (0, 0), (-1, -1), 1, colors.black), 
-        ("GRID", (0, 0), (4, 4), 1, colors.black), 
-        ("BACKGROUND", (0, 0), (3, 0), colors.gray), 
-        ("TEXTCOLOR", (0, 0), (-1, 0), colors.whitesmoke), 
-        ("ALIGN", (0, 0), (-1, -1), "CENTER"), 
-        ("BACKGROUND", (0, 1), (-1, -1), colors.beige), 
+        ("BACKGROUND", (0, 0), (-1, 0), colors.gray),
+        ("TEXTCOLOR", (0, 0), (-1, 0), colors.whitesmoke),
+        ("ALIGN", (0, 0), (-1, -1), "CENTER"),
+        ("FONTNAME", (0, 0), (-1, -1), "Helvetica-Bold"),
+        ("GRID", (0, 0), (-1, -1), 1, colors.black),
+        ("BACKGROUND", (0, 1), (-1, -1), colors.white),
     ]) 
 
-    # Creates a table object and passes the style to it 
+    # Creating a table object and passing the style to it 
     table = Table(DATA, style=style) 
 
-    # Final step which builds the actual pdf putting together all the elements 
     pdf.build([title, table])
 
-# Call the function to generate receipt
 generate_receipt()
