402ffccf40f465f23c92a5f5c66f9eccb998603a
vinay961 <vinay100rai@gmail.com>
2024-06-27 10:41:56 +0530

    fileSystem for text

---
+++ Practice/FileSystem/name.py
@@ -0,0 +1,25 @@
+
+# writing the content in the file
+
+# file = open("names.txt","a")
+# file.write(f"{name}\n")
+
+
+# for _ in range(3):
+#     name = input("What's your name? ")
+    
+#     with open("names.txt","a") as file:
+#         file.write(f"{name}\n")
+
+
+# Reading the content of file
+
+# names = []
+
+# with open("names.txt","r") as file:
+#     for line in file:
+#         names.append(line)
+
+# for name in sorted(names):
+#     print(f"Hello, {name.rstrip()}")
+
